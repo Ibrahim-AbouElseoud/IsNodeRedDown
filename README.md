bb2357f0335cc362072305b9eeb7758e6f13e01f
Ibrahim Abou Elseoud <Ibrahim.elseoud@gmail.com>
2016-08-20 08:45:57 +0200

    code base and resources

---
+++ README.md
@@ -0,0 +1,5 @@
+# Is Node Red Down ?? <br>
+
+Just a simple web app that sees if our Internship's IBM Bluemix's Node Red server is down or not and reflects so to the user. Additionally there is a server api reached by  /status that gives either true if down or false if not down.<br> <br>
+
+Inspired by the popular office phrase <b>"Is Node Red down again ??"  </b> , well now you can check ;)
---
+++ server.js
@@ -0,0 +1,30 @@
+var express       = require('express');
+var app           = express();
+var morgan = require('morgan');
+var request = require("request");
+
+
+app.use(morgan('tiny'));
+
+app.listen(3000, function(){
+  console.log('listening on port '+ 3000);
+});
+
+app.use(express.static('public'));
+
+app.get('/status',function(req,res){
+  console.log(req.body);
+  urli='http://drivepal.mybluemix.net';
+    request({
+        url: urli,
+        method: 'GET',
+    }, function (error, response, body) {
+      console.log(response.statusCode);
+        if(error) {console.log(error); res.send(true);}
+        else if ( response.statusCode === 200) { res.send(false);} //not down
+        else {
+          res.send(true);
+        }
+
+  });
+})
